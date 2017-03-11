94239226e2e5007ad93b3be7e373c3c7a84dab30
Geoff Davis <geoffdavis92@gmail.com>
2017-03-10 23:56:40 -0600

    fixed flow config, trimmed whitespace in import section

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,12 @@
 // core @flow
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-
 import {
 	Router,
 	Route,
 	IndexRoute,
 	browserHistory
 } from 'react-router'
-
 import AppWrapper from './AppWrapper'
 import Home from './views/Home'
 
