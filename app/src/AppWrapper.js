d9b0a30445f57acda4ec45da7cc92904bf0af4fc
Geoff Davis <geoffdavis92@gmail.com>
2017-03-10 23:19:24 -0600

    add baseline template files

---
+++ app/src/AppWrapper.js
@@ -0,0 +1,5 @@
+// AppWrapper @flow
+import React, { Component } from 'react'
+import './styles/AppWrapper.css'
+
+export default class AppWrapper extends Component { render() { return <p>AppWrapper</p> } }
---
+++ app/src/views/Home.js
@@ -0,0 +1,5 @@
+// views/Home @flow
+import React, { Component } from 'react'
+import '../styles/Home.css'
+
+export default class Home extends Component { render() { return <p>Home</p> } }
